c5edef2fd6503a4481aadeb6aa64cddc4959e811
kpow07 <kpow@shaw.ca>
2021-10-27 13:33:57 -0600

    updated files

--- model/zoo.js
+++ model/zoo.js
@@ -1,33 +1,33 @@
 let habitats = [
-    { name: 'forest', description: 'Forests are often found in cool areas in the Northern Hemisphere, such as Canada, Alaska, Scandinavia, and parts of Russia. They are dominated by evergreen fir trees, and are home to animals such as moose, elk, beavers, and wolves.',
+    { name: 'forest', description: 'Forests are often found in cool areas in the Northern Hemisphere, such as Canada, Alaska, Scandinavia, and parts of Russia. They are dominated by evergreen fir trees, and are home to animals such as moose, elk, beavers, and wolves. ',
         animals: [ 
         { type: 'bear', clues: ['I eat a diet of mainly fish and berries.', 'I spend 4 to 6 months of the year in a deep sleep.', 'I work part-time at A&W as their loveable mascot, Rooty.' ]},
         { type: 'deer', clues: ['I have eyes on the side of my head.', 'A male of my species is called a buck.', 'A similar species to mine propels Santa\'s sleigh.' ]},
         { type: 'wolf', clues: ['I travel in packs.', 'I am the largest member of the canine family.', 'Ah-woooooo! I love to howl at the moon.' ]},
         { type: 'beaver', clues: ['I have really big teeth.', 'I can be found on a Canadian nickel.', 'Some people eat my tail as a dessert.' ]},
     ]}, 
-    { name: 'grassland', description: 'The tropical grasslands are found in Africa. They are home to huge herds of grass-eating animals, and powerful predators that feed on them.', 
+    { name: 'grassland', description: 'The tropical grasslands are found in Africa. They are home to huge herds of grass-eating animals, and powerful predators that feed on them. ', 
     animals: [ 
         { type: 'giraffe', clues: ['I am a herbivore that can guzzle up to 45kg of leaves and twigs per day!', 'I am the tallest land animal.', 'Our spots are like human fingerprints, no two individual animals have the same pattern.' ]},
         { type: 'lion', clues: ['I live in a group called a pride.', 'I am often the mane attraction.',  'I shall call him, Simba.' ]},
         { type: 'elephant', clues: ['I am constantly eating.', 'I can communicate through vibrations.', 'I never forget.' ]},
         { type: 'zebra', clues: ['I can run at speeds up to 65km per hour.', 'I am currently classified as endangered. Sad face.', 'My unique colouring helps to camouflage me from predators.' ]},
     ]}, 
-    { name: 'desert', description: 'Deserts are dry places where little plant life can grow. Hot deserts may be barren areas of rock and sand, but some support animals and plants that can cope with very little water.',
+    { name: 'desert', description: 'Deserts are dry places where little plant life can grow. Hot deserts may be barren areas of rock and sand, but some support animals and plants that can cope with very little water. ',
     animals: [ 
         { type: 'camel', clues: ['I will spit if you make me mad!', 'I have three sets of eyelids and two rows of eyelashes to keep sand out of my eyes.', 'The most famous of my species was called Sally.' ]},
         { type: 'snake', clues: ['I do not have eyelids.', 'I smell with my tongue.', 'You might find me slithering in your garden, catching me a mouse.' ]},
         { type: 'horned lizard', clues: ['I am a cold-blooded species.', 'I have large pointed scales that protrude from my back.', 'Some call me Liz for short.' ]},
         { type: 'scorpion', clues: ['I may be the oldest land animal, fossil records suggest I was here even before the dinosaurs!', 'I am a member of the arachnid family.', 'I can control how much venom to release with my stinger.' ]},
     ]}, 
-    { name: 'mountain', description: 'The tops of high mountain ranges are extremely cold, and few plants grow there. Animals living in these high places must cope with cold, lack of food, and steep, rocky terrain.',
+    { name: 'mountain', description: 'The tops of high mountain ranges are extremely cold, and few plants grow there. Animals living in these high places must cope with cold, lack of food, and steep, rocky terrain. ',
     animals: [ 
         { type: 'mountain goat', clues: ['I can jump 12 feet in one leap!', 'Males are called billies.', 'Both males and females have beards and horns.' ]},
         { type: 'mountain lion', clues: ['When in danger, I may climb a tree to escape.', 'I am very solitary and will avoid encounters, sometimes I am referred to as a "ghost".', 'Bubbles had one named Steve French.' ]},
         { type: 'wolverine', clues: ['I am little but fierce, with sharp teeth and retractable claws.', 'I am a member of the weasel family.', 'Played by Hugh Jackman several times.' ]},
         { type: 'bighorn sheep', clues: ['The female of my species is called an ewe.', 'The males of my species have long spiral horns.', 'I am on the Dodge emblem.' ]},
     ]}, 
-    { name: 'aquatic', description: 'Oceans and seas form the largest habitat on the planet. A large variety of animals live in the marine habitat, from tiny plankton to the largest animal in the world - the blue whale.',
+    { name: 'aquatic', description: 'Oceans and seas form the largest habitat on the planet. A large variety of animals live in the marine habitat, from tiny plankton to the largest animal in the world - the blue whale. ',
     animals: [ 
         { type: 'shark', clues: ['I have no bones in my body.', 'My species is estimated to have been around for over 455 million years.', 'There is a whole week dedicated to me on TV.' ]},
         { type: 'sea lion', clues: ['I can hold my breath underwater from 8 to 20 minutes!', 'I spend time both on land and in the sea.', 'My name might fool you, I am not the King of the Jungle.' ]},
@@ -38,7 +38,7 @@ let habitats = [
 
 
 let randomAnimal 
-let visitorLocation
+let zookeeperLocation
 
 // Starts game with random animal selection
 function chooseRandomAnimal() {
@@ -62,16 +62,15 @@ function listHabitats() {
     return habitatList
 }
 
-// Explore each habitat & creatures within
+// Explore each habitat futher to see list of animals inside
 function exploreHabitat(habitats) {
-    visitorLocation = habitats.name.description
+    zookeeperLocation = habitats.name.description
     console.log('The zookeeper has entered the: ', habitats)
-    console.log(habitats.description)
 }
 
-// Is the random creature in this habitat?
-function searchHabitat() {
-    return (randomAnimal.type === visitorLocation)
+// Select the animal you think fits the clues given to win the game!
+function selectAnimal() {
+    return (randomAnimal.type === zookeeperLocation)
 }
 
 module.exports = {
@@ -79,5 +78,5 @@ module.exports = {
     chooseRandomAnimal,
     getClue,
     exploreHabitat,
-    searchHabitat,
+    selectAnimal,
 }
--- routes/zooRoutes.js
+++ routes/zooRoutes.js
@@ -13,36 +13,40 @@ router.get('/chooseRandomAnimal', (req, res) => {
 })
 
 router.get('/getClue', (req, res) => {
+    zoo.getClue()
     clueToSend = randomAnimal.clues[clue]
     clue++
     if ([clue] > 3) {
         res.send('Oops, you have run out of clues! Keep exploring, Zookeeper!')
     } else {
-    res.send('Your first clue is: ' + clueToSend + ' To view the habitats you can explore, continue to /listHabitats. For up to 2 additional clues revisit /getClue.')
+    res.send('Your clue is: ' + clueToSend + ' To view the habitats you can explore, continue to /listHabitats. For up to 2 additional clues revisit /getClue.')
 }})
 
 router.get('/listHabitats', (req, res) => {
-    res.send(zoo.listHabitats()) 
-    // res.send('To further explore the habitats and find more about the creatures within, continue to /exploreHabitats.')
+    res.json(zoo.listHabitats())
+    // HELPPPP this won't work
+    // res.send('To further explore the habitats and find more about the creatures within, continue to /exploreHabitats.') 
 })
 
 
 router.get('/exploreHabitat', (req, res) => {
     res.send('There are 5 habitats: forest, grasslands, desert, mountain, and aquatic. To explore each habitat further, use req query. Example: /exploreHabitat?habitats=forest')
     let habitats = req.query.habitats
-    // zoo.exploreHabitat(habitats)
+    zoo.exploreHabitat(habitats)
     res.send('You have entered the ' + habitats + 'habitat!')
 })
 
-router.get('/searchHabitat', (req, res) => {
+
+router.get('/selectAnimal', (req, res) => {
     let message
-    let found = zoo.searchHabitat(randomAnimal)
+    let found = zoo.selectAnimal(randomAnimal)
     if (found) {
         message = 'Wow! You found the ' + randomAnimal + 'You will make a wonderful Zookeeper!'
     } else {
         message = 'Uh oh! This is not the correct animal enclosure. You still have some work to do, aspiring Zookeeper! Visit /getClue to get another clue.'
     }
     res.send(message)
+    // implement 3 guesses before game ends
 })
 
 router.get('/', (req, res) => {
@@ -50,10 +54,11 @@ router.get('/', (req, res) => {
   `Welcome to the Zoo, do you think you have what it takes to be a Zookeeper? <br />
    In this game your knowledge will be tested using a series of clues to locate a random animal in one of our 5 habitats! <br />
    To start a game go to /chooseRandomAnimal <br />
-   To receive additional clues go to /getClue <br />
+   To receive up to 3 clues go to /getClue <br />
    For a list of the 5 available habitats go to /listHabitats <br />
-   For more information on each habitat go to /exploreHabitat <br />
-   To select the animal which you think best reflects the clues given go to /searchHabitat <br />
+   For more information on the animals within each habitat go to /exploreHabitat <br />
+   To select the animal which you think best reflects the clues given go to /selectAnimal <br />
+   You will have 3 chances to guess the correct animal! <br />
    Good luck, and don't be a cheetah!`
 
   res.send(instructions)
